c5a5811a30c8ef0b09153d3f6a2e90835e05c795
Siddhubollam9 <siddharthbollam4@gmail.com>
2025-02-16 14:53:15 +0530

    Fixed ALLOWED_HOSTS for Render

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p#se7#mta2e3tgm!_t504t1a_=kdr79is=sbn8+g()zt8cqifq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
